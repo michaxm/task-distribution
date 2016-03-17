bbc29bd1ee83c4e2f5251f282b53249d01441f33
Axel Mannhardt <notset@example.com>
2016-03-17 12:32:00 +0100

    documentation of task type

--- src/Control/Distributed/Task/Types/TaskTypes.hs
+++ src/Control/Distributed/Task/Types/TaskTypes.hs
@@ -2,6 +2,7 @@ module Control.Distributed.Task.Types.TaskTypes where
 
 import qualified Data.ByteString.Lazy as BLC
 
+-- | This is a small common ground for some assumptions: input is a list of sth., deserialization is the responsibility of the task itself.
 type TaskInput = [BLC.ByteString]
-type TaskResult = [BLC.ByteString]
+type TaskResult = TaskInput
 type Task = TaskInput -> TaskResult
